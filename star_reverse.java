8afbe283a0b1f38fb0cd32f51a543fef3136ef3e
[haileymoon] <[moongirl1148@naver.com]>
2021-09-30 18:05:09 +0900

    tutoring second session(function_for)

---
+++ star_reverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class second_practice_star_reverse {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int num = scanner.nextInt(); 
+    for(int i = 0; i<num; i++){
+        for(int x = num-1; x>i; x--){
+            System.out.print(" ");
+        }
+        for(int j = 0; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    scanner.close();
+    }
+}
