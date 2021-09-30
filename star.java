45eb1efcd7e6649d5ac0984c617b35fd04fb7d31
[haileymoon] <[moongirl1148@naver.com]>
2021-09-30 18:42:51 +0900

    #2438번

--- star.java
+++ star.java
@@ -7,7 +7,7 @@ class second_practice_star {
     for(int i = 0; i<num; i++){
         for(int j = 0; j<=i; j++){
             System.out.print("*");
-        }
+        } 
         System.out.println();
     }
     scanner.close();
